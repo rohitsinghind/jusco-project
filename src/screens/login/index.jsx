11b15e47d303f754cf95ef66799dae04111513f8
rohit <rohitsinghind22@gmail.com>
2022-07-28 21:27:47 +0530

    fisrt commit

---
+++ src/screens/login/index.jsx
@@ -0,0 +1,169 @@
+import React,{useState} from 'react'
+import { styles } from './styles';
+import { Link } from "react-router-dom";
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from "@mui/material/Typography";
+import TextField from '@mui/material/TextField';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+
+export default function Login() {
+
+  const [creds, setCreds] = useState({ username: '', password: '' });
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleChange = (key) => {
+    key.preventDefault();
+    setCreds({ ...creds, [key.target.id]: key.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+  };
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    
+  }
+
+  const [respond, setRespond] = useState("")
+  
+  const mediaQuery = window.matchMedia("(max-width: 550px)");
+
+  return (
+    <>
+        <Container maxWidth="xl" sx={styles.container}>
+          <Paper sx={styles.paper} variant="outlined" >
+            <img  style={styles.imgLogo} src={require('../../assets/image/logo.png')} alt="" srcset="" />
+            <Typography
+                sx={styles.textLogo} >
+                Bulk Generation System
+            </Typography>
+            <Typography
+                sx={styles.loginText} >
+                Log in to continue.
+            </Typography>
+            <TextField
+          id="username"
+        type="text"
+          label="Enter your Username"
+          placeholder="Username"
+          value={creds.username || ''}
+          onChange={handleChange}
+          sx={styles.inputField}
+        />
+        <FormControl variant="outlined" sx={styles.inputField}>
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            placeholder="*****"
+            type={showPassword ? 'text' : 'password'}
+            value={creds.password || ''}
+            onChange={handleChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        <Button 
+        variant="contained" 
+        sx={styles.loginBtn} 
+        onClick={loginHandler}
+        >Log in
+        </Button>
+        <Divider sx={styles.divider}/>
+            <Typography
+                sx={styles.forgotPwdText} >
+                Forgot your password? 
+            </Typography>
+          </Paper>
+        </Container>
+
+     {/* <Container maxWidth="xl" sx={mediaQuery.matches?styles.mobileContainer:""}>
+     <Typography>{respond?respond:""}</Typography>
+        <Box sx={styles.box}>
+            <div>
+            <Paper sx={} variant="outlined" sx={{ p: 4 }}>
+            <img  style={styles.imgLogo} src={require('../../assets/image/logo.png')} alt="" srcset="" />
+            <Typography
+                sx={styles.textLogo} >
+                Digital Bill Submission and Tracking System
+            </Typography>
+            <Typography
+                sx={styles.loginText} >
+                Log in to continue.
+            </Typography>
+            <TextField
+          id="email"
+        type="text"
+          label="Enter your Email"
+          placeholder="Email"
+          value={creds.email || ''}
+          onChange={handleChange}
+          sx={styles.center}
+        />
+        <div style={styles.center}>
+        <FormControl variant="outlined" sx={{mt:2, width: '120%'}}>
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="password"
+            placeholder="*****"
+            type={showPassword ? 'text' : 'password'}
+            value={creds.password || ''}
+            onChange={handleChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        </div>
+        <div style={styles.center}>
+        <Button 
+        variant="contained" 
+        sx={styles.loginBtn} 
+        onClick={loginHandler}
+        >Log in
+        </Button>
+        </div>
+        <Divider sx={styles.divider}/>
+            <Typography
+                sx={styles.signupText1} >
+                Don't have an account? 
+            </Typography>
+      </Paper>
+            </div>
+            <Box>
+          </Box>
+          </Box>
+      </Container> */}
+    </>
+  )
+}
---
+++ src/screens/login/styles.js
@@ -0,0 +1,61 @@
+export const styles = {
+    container:{
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        background:"#f0f2f7",
+        height:"100vh"
+    },
+    paper:{
+        display:"flex",
+        flexDirection:"column",
+        alignItems:"center",
+        p:4,
+        width:"450px"
+    },
+    imgLogo:{
+        display:"flex",
+        justifyContent:"center",
+        width:"100%",
+        marginBottom:"10px"
+    },
+   
+    textLogo:{
+        fontFamily: "'Open Sans', sans-serif",
+        fontSize:"28px",
+        color:"#2D2D2D",
+        display:"flex",
+        justifyContent:"center",
+        my:"20px",
+    },
+    loginText:{
+        fontFamily: "'Open Sans', sans-serif",
+        fontSize:"25px",
+        color:"#5D5D5D",
+        display:"flex",
+        justifyContent:"center",
+        mb:"10px"
+    },
+    inputField:{
+        width:"100%",
+        my:"10px"
+    },
+
+    divider:{
+        background:"#e1e1e1",
+        my:"20px",
+    },
+
+    forgotPwdText:{
+        color:"#0044a9",
+        fontFamily: "'Open Sans', sans-serif",
+        fontSize:"20px",
+        cursor:"pointer",
+    },
+    loginBtn:{
+        width:"100%",
+        background:"#1b84e7",
+        mt:"20px",
+    },
+
+  };
